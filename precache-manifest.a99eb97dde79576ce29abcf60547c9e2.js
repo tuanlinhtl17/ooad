759c5c26e4d904a59d697b7438c181cafacb25d4
tl17 <tuanlinhtl17@gmail.com>
2019-05-27 23:29:54 +0700

    Updates

---
+++ precache-manifest.a99eb97dde79576ce29abcf60547c9e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28593a07b2b28eb6f431ef401aa0d697",
+    "url": "/ooad/index.html"
+  },
+  {
+    "revision": "165b50632ff6a8c07b0b",
+    "url": "/ooad/static/css/main.cf3e0be7.chunk.css"
+  },
+  {
+    "revision": "436bdb1ac13dec9ad9cd",
+    "url": "/ooad/static/js/2.9bd61ce7.chunk.js"
+  },
+  {
+    "revision": "165b50632ff6a8c07b0b",
+    "url": "/ooad/static/js/main.81221771.chunk.js"
+  },
+  {
+    "revision": "2d0203bc801b0c11a3c7",
+    "url": "/ooad/static/js/runtime~main.021ef5c0.js"
+  }
+]);
